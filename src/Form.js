3fbb93b9a9aee3822a2d2abbbd38bdd21294947a
Diego Castro <diegojcr4@gmail.com>
2021-11-10 18:41:17 -0500

    add basic form component

---
+++ src/Form.js
@@ -0,0 +1,28 @@
+import { React, useState } from 'react';
+
+export default function Form() {
+  const [formVals, setFormVals] = useState({});
+
+  const handleNameChange = (e) => {
+    const name = e.target.value;
+    setFormVals(prev => ({...prev, name}))
+  }
+  
+  const handleAgeChange = (e) => {
+    const age = e.target.value;
+    setFormVals(prev => ({...prev, age}))
+  }
+
+  return(
+    <form onSubmit={e => e.preventDefault()}>
+      <label>
+        Name:
+        <input type="text" value={formVals.name} onChange={handleNameChange}/>
+      </label>
+      <label>
+        Age:
+        <input type='number' value={formVals.age} onChange={handleAgeChange} />
+      </label>
+    </form>
+  )
+};
